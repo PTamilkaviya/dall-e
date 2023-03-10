8be2a21697865bf0e247052cc017f18f5539ee1c
Tamilkaviya <tamilkaviya27@gmail.com>
2023-03-10 15:14:45 +0530

    first commit

---
+++ server/mongodb/connect.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+
+
+const connectDB = (url) => {
+
+  mongoose.set('strictQuery', true)
+
+  mongoose.connect(url,{ useNewUrlParser: true,useUnifiedTopology: true })
+      .then(() => console.log('connected to mongo'))
+    .catch((err) => {
+      console.error('failed to connect with mongo');
+      console.error(err);
+      // mongoose.set('useFindAndModify', false)
+      // process.exit(1) 
+    });
+};
+
+export default connectDB;                                 
